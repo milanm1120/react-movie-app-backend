26ca9bdc43e19dc98bfc5fff2c12bf72d3364728
Milan <milanm1120@gmail.com>
2022-02-07 11:58:39 -0500

    build migrations and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :ratings
+  resources :comments
+  resources :reviews
   devise_for :users
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
